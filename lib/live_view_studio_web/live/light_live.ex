21a044e56b230d7306f17bb8edeebb521985f877
Nick Olson <naolson.1819@gmail.com>
2023-10-25 08:16:52 -0500

    completed first module

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,62 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, brightness: 10)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style={"width: #{@brightness}%"}>
+          <%= @brightness %>%
+        </span>
+      </div>
+      <button phx-click="off">
+        <img src="/images/light-off.svg" />
+      </button>
+      <button phx-click="down">
+        <img src="/images/down.svg" />
+      </button>
+      <button phx-click="up">
+        <img src="/images/up.svg" />
+      </button>
+      <button phx-click="on">
+        <img src="/images/light-on.svg" />
+      </button>
+      <button phx-click="fire">
+        <img src="/images/fire.svg" />
+      </button>
+    </div>
+    """
+  end
+
+  def handle_event("on", _params, socket) do
+    {:noreply, assign(socket, brightness: 100)}
+  end
+
+  def handle_event("off", _params, socket) do
+    {:noreply, assign(socket, brightness: 0)}
+  end
+
+  def handle_event("up", _params, socket) do
+    # &() is an anonymous function
+    # &1 is the first argument in this case brightness
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _params, socket) do
+    socket = update(socket, :brightness, &max(&1 - 10, 0))
+    {:noreply, socket}
+  end
+
+  def handle_event("fire", _params, socket) do
+    # brightness should be a random number between 0 and 100
+    brightness = Enum.random(0..100)
+    {:noreply, assign(socket, brightness: brightness)}
+  end
+end
