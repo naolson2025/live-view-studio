c9233fe3840a2ccc2c6748dfc73ffc64e14d1eb1
Nick Olson <naolson.1819@gmail.com>
2023-11-07 07:18:34 -0600

    chapter 9 video

--- lib/live_view_studio_web/live/boats_live.ex
+++ lib/live_view_studio_web/live/boats_live.ex
@@ -17,7 +17,7 @@ defmodule LiveViewStudioWeb.BoatsLive do
     ~H"""
     <h1>Daily Boat Rentals</h1>
     <div id="boats">
-      <form>
+      <form phx-change="filter">
         <div class="filters">
           <select name="type">
             <%= Phoenix.HTML.Form.options_for_select(
@@ -60,6 +60,13 @@ defmodule LiveViewStudioWeb.BoatsLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("filter", %{"type" => type, "prices" => prices}, socket) do
+    filter = %{type: type, prices: prices || []}
+    boats = Boats.list_boats(filter)
+
+    {:noreply, assign(socket, boats: boats, filter: filter)}
   end
 
   defp type_options do
